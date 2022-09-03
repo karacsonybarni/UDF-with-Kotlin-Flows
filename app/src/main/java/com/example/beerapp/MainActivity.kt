a9efec3ab433dedb1c513ccfb8ad8952aae25003
barni <barna.karacsony@geogebra.org>
2022-09-03 09:49:54 +0200

    init

---
+++ app/src/main/java/com/example/beerapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.beerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.beerapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
